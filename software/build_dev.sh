1fb6c0cbae470421d80ab16e43641d290ab0afa8
kabel-knrd <kongo.kellner@gmail.com>
2024-03-21 14:45:24 +0100

    Copied build.sh to build_dev.sh so Docker-Containers can now be build for AMD64-Architecture and can run/be tested on dev-PC.

---
+++ software/build_dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+cd "$(dirname "$0")"
+
+name=cybicsbuilder
+docker buildx ls | grep -q $name || docker buildx create --driver-opt network=host --use --config config.toml --name $name
+docker buildx inspect --bootstrap
+
+docker buildx build --platform linux/amd64 -t localhost:5000/cybics-readi2c:latest --push ./scripts
+docker buildx build --platform linux/amd64 -t localhost:5000/cybics-openplc:latest --push ./OpenPLC
+docker buildx build --platform linux/amd64 -t localhost:5000/cybics-opcua:latest --push ./opcua
+docker buildx build --platform linux/amd64 -t localhost:5000/cybics-fuxa:latest --push ./FUXA
+docker buildx build --platform linux/amd64 -t localhost:5000/cybics-stm32:latest --push ./stm32
